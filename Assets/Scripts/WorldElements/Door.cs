7a0e1dccc0d3360d108c42aa95bfc193f6f52d04
Andres Vieira <anvieiravazquez@gmail.com>
2021-01-17 00:14:13 +0100

    Door: Fixes door not triggering OnTriggerEnter2D
    Turns out one of the 2 colliding entities has to have a RigidBody2D of
    some sort. Preferrably kinematic, so that BodyType is set at the first
    frame of the game.

--- Assets/Scripts/WorldElements/Door.cs
+++ Assets/Scripts/WorldElements/Door.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class Door : MonoBehaviour
 {
     DoorDestination m_doorDestination;
+    Rigidbody2D m_rigidBody;
 
     void Start()
     {
         m_doorDestination = GetComponentInChildren<DoorDestination>();
         if (m_doorDestination == null) throw new UnityException("Component Door has no DoorDestination in its children");
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        if (m_rigidBody == null) throw new UnityException("Component Door has no RigidBody2D in its children");
+        m_rigidBody.bodyType = RigidbodyType2D.Kinematic;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
